ff12e305cf60c3e8a5b27d31b5741f1e2311d889
913-Gog-Oana <oana.gog@stud.ubbcluj.ro>
2024-03-17 16:37:50 +0200

    refactor

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,25 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "prettier",
-  ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "prettier"],
-  rules: {
-    "prettier/prettier": ["error"],
-    "@typesript-eslint/no-unused-vars": ["error"],
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
+    root: true,
+    env: {browser: true, es2020: true},
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'prettier',
     ],
-  },
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh', 'prettier'],
+    rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
+    },
+
+    parserOptions: {
+        ecmaVersion: 7,
+        ecmaFeatures: {
+            modules: true,
+            jsx: true,
+        },
+    },
 };
